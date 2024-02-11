7d7f35956f725fe429c6e5aa12341e08da7fcd55
MihkelPLoompuu <158562003+MihkelPLoompuu@users.noreply.github.com>
2024-02-11 14:25:07 +0200

    Add files via upload

---
+++ Homework/Ul-id/ul 1.cs
@@ -0,0 +1,14 @@
+﻿namespace Ul1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = Console.ReadLine();
+            string y = Console.ReadLine();
+            double tulemus = x * (y / 100);
+
+            Console.WriteLine($"{y}% arvust {x} on {tulemus}");
+        }
+    }
+}
---
+++ Homework/Ul-id/ul4.cs
@@ -0,0 +1,23 @@
+﻿namespace Ul4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int targetNumber = random.Next(1, 10);
+
+            int guess = 10;
+            while (guess != targetNumber)
+            {
+                Console.Write("Guess the number im thinking of: ");
+                guess = int.Parse(Console.ReadLine());
+
+                if (guess != targetNumber)
+                    Console.WriteLine("That is not the number im thinking:");
+                else
+                    Console.WriteLine("Correct, you guessed my number: " + targetNumber);
+            }
+        }
+    }
+}
