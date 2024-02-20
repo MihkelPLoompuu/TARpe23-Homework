25af2c1acdc5c82735e8740355993140d6feb255
MihkelPLoompuu <158562003+MihkelPLoompuu@users.noreply.github.com>
2024-02-20 16:52:21 +0200

    Add files via upload

---
+++ Homework/Ul-id ND2/UL3.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+
+namespace UL3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the square: ");
+            int S = Convert.ToInt32(Console.ReadLine());
+
+            WriteSquareOfNumbers(S);
+        }
+
+        static void WriteSquareOfNumbers(int number)
+        {           
+            string mm = new string((char)(number + '0'), number);
+
+            using (StreamWriter writer = new StreamWriter("text.txt"))
+            {
+                for (int i = 0; i < number; i++)
+                {
+                    writer.WriteLine(mm);
+                }
+            }
+
+            Console.WriteLine($"{number} on selles failis text.txt");
+        }
+    }
+
+}
+
+           
+        
