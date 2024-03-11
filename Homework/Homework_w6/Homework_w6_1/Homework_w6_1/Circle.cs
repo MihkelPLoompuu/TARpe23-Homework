050617285881685a8ab775ce5474ae761ba176e4
MihkelPLoompuu <158562003+MihkelPLoompuu@users.noreply.github.com>
2024-03-11 17:18:49 +0200

    Add files via upload

---
+++ Homework/Homework_w6/Homework_w6_1/Homework_w6_1/Circle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_w6_1
+{
+    internal class Circle
+    {
+        public double Radius;
+        public double Diameter;
+        public double Radiusend;
+        public double MM;
+        private double diameter()
+        {
+            try
+            {
+                Console.Write("Enter diameter : ");
+                double A = Convert.ToInt32(Console.ReadLine());
+                Diameter = A;
+                return Diameter;                
+            }
+            catch 
+            {
+              Console.WriteLine("Valus are not set");
+            }
+            return Diameter;
+        }
+        private double radius()
+        {
+            Radius = Diameter;
+            double B = Radius / 2;
+            //Console.WriteLine($"radius is : {B} cm");
+            Radiusend = B;
+            return Radiusend;
+        }
+        public double Circumeference() 
+        {
+            double C = 2 * 3.14 * Radiusend;
+            //Console.WriteLine(C);
+            MM = C;
+            return MM;
+        } 
+        public Circle() 
+        {
+            diameter();
+            radius();
+            Circumeference();
+            Console.WriteLine($"Diameter is : {Diameter} cm , radius is : {Radiusend} cm and Circumeference" +
+                $"is {MM} cm");
+        } 
+    }
+}
---
+++ Homework/Homework_w6/Homework_w6_1/Homework_w6_1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Homework_w6_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle radius = new Circle();
+        }
+    }
+}
