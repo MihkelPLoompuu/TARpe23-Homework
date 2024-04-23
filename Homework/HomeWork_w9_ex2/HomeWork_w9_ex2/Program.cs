e05669dfdc934174701d81459121c8d89fd49114
MihkelPLoompuu <158562003+MihkelPLoompuu@users.noreply.github.com>
2024-04-23 20:41:13 +0300

    Add files via upload

---
+++ Homework/HomeWork_w9_ex2/HomeWork_w9_ex2/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace HomeWork_w9_ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat();
+            cat.CatIsPlaying(false,25);
+        }
+    }
+}
