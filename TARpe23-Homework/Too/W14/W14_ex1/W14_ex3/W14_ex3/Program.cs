5901162ebba226bbc1a1f9d68ccbbc111a59b07d
MihkelPLoompuu <158562003+MihkelPLoompuu@users.noreply.github.com>
2024-05-16 19:35:57 +0300

    H
    H

---
+++ TARpe23-Homework/Too/W14/W14_ex1/W14_ex3/W14_ex3/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace W14_ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sorting();
+        }
+        static void Sorting()
+        {          
+            int[] numbers = GenerateRandom(10, 0, 1000);
+            Console.WriteLine("Unsorted:");
+            Array(numbers);
+            InsertionSort(numbers);
+            Console.WriteLine("Sorted:");
+            Array(numbers);
+        }
+        static int[] GenerateRandom(int size, int min, int max)
+        {
+            Random rand = new Random();
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = rand.Next(min, max);
+            }
+            return array;
+        }
+        static void InsertionSort(int[] array)
+        {
+            int n = array.Length;
+            for (int i = 1; i < n; i++)
+            {
+                int key = array[i];
+                int j = i - 1;
+                while (j >= 0 && array[j] > key)
+                {
+                    array[j + 1] = array[j];
+                    j = j - 1;
+                }
+                array[j + 1] = key;
+            }
+        }
+        static void Array(int[] array)
+        {
+            foreach (int num in array)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
