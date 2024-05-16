906861fc82b830e32239ddd560d0ab142555610b
MihkelPLoompuu <158562003+MihkelPLoompuu@users.noreply.github.com>
2024-05-16 19:34:58 +0300

    H
    H

---
+++ TARpe23-Homework/Too/W14/W14_ex1/W14_ex1/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace W14_ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Diamond();
+        }
+        static void Diamond() 
+        {
+            int i, j, count = 1, number;
+            Console.Write("Enter number of rows:");
+            number = int.Parse(Console.ReadLine());
+            count = number - 1;
+            for (j = 1; j <= number; j++)
+            {
+                for (i = 1; i <= count; i++)
+                    Console.Write(" ");
+                count--;
+                for (i = 1; i <= 2 * j - 1; i++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+            count = 1;
+            for (j = 1; j <= number - 1; j++)
+            {
+                for (i = 1; i <= count; i++)
+                    Console.Write(" ");
+                count++;
+                for (i = 1; i <= 2 * (number - j) - 1; i++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+        }
+    }
+}
